8b1655d725da075f5afb5d691d04d8cbd4e0deae
ExoticAmmu <izavhishek@gmail.com>
2022-07-28 15:56:48 +0530

    minor changes

--- routes/web.php
+++ routes/web.php
@@ -82,7 +82,290 @@ Route::prefix('admin')->group(function(){
         Route::get('view-user/{id}',[DashboardController::class, 'view'])->name('admin.users.view');
 
         // Admin Tailstack
-        
+        Route::get('index-analytics', function(){
+            return View('admin.tailstack.index-analytics');
+        });
+        Route::get('index-ecommerce', function(){
+            return View('admin.tailstack.index-ecommerce');
+        });
+        Route::get('index-projects', function(){
+            return View('admin.tailstack.index-projects');
+        });
+        Route::get('index-crm', function(){
+            return View('admin.tailstack.index-crm');
+        });
+        Route::get('index-hosting', function(){
+            return View('admin.tailstack.index-hosting');
+        });
+        Route::get('calendar', function(){
+            return View('admin.tailstack.calendar');
+        });
+        Route::get('ecommerce/product', function(){
+            return View('admin.tailstack.ecommerce.product');
+        });
+        Route::get('ecommerce/product-detail', function(){
+            return View('admin.tailstack.ecommerce.product-detail');
+        });
+        Route::get('ecommerce/product-detail', function(){
+            return View('admin.tailstack.ecommerce.product');
+        });
+        Route::get('ecommerce/order', function(){
+            return View('admin.tailstack.ecommerce.order');
+        });
+        Route::get('ecommerce/order-detail', function(){
+            return View('admin.tailstack.ecommerce.order-detail');
+        });
+        Route::get('ecommerce/customers', function(){
+            return View('admin.tailstack.ecommerce.customers');
+        });
+        Route::get('ecommerce/cart', function(){
+            return View('admin.tailstack.ecommerce.cart');
+        });
+        Route::get('ecommerce/checkout', function(){
+            return View('admin.tailstack.ecommerce.checkout');
+        });
+        Route::get('ecommerce/seller', function(){
+            return View('admin.tailstack.ecommerce.checkout');
+        });
+        Route::get('ecommerce/invoice', function(){
+            return View('admin.tailstack.ecommerce.checkout');
+        });
+        Route::get('project/list', function(){
+            return View('admin.tailstack.project.list');
+        });
+        Route::get('project/detail', function(){
+            return View('admin.tailstack.project.detail');
+        });
+        Route::get('project/kanban', function(){
+            return View('admin.tailstack.project.kanban');
+        });
+        Route::get('project/create', function(){
+            return View('admin.tailstack.project.create');
+        });
+        Route::get('authentication/register-basic', function(){
+            return View('admin.tailstack.authentication.register-basic');
+        });
+        Route::get('authentication/register-cover', function(){
+            return View('admin.tailstack.authentication.register-cover');
+        });
+        Route::get('authentication/register-ilustration', function(){
+            return View('admin.tailstack.authentication.register-ilustration');
+        });
+        Route::get('authentication/login-basic', function(){
+            return View('admin.tailstack.authentication.login-basic');
+        });
+        Route::get('authentication/login-cover', function(){
+            return View('admin.tailstack.authentication.login-cover');
+        });
+        Route::get('authentication/login-ilustration', function(){
+            return View('admin.tailstack.authentication.login-ilustration');
+        });
+        Route::get('authentication/forgot-password', function(){
+            return View('admin.tailstack.authentication.forgot-password');
+        });
+        Route::get('authentication/confirm-email', function(){
+            return View('admin.tailstack.authentication.confirm-email');
+        });
+        Route::get('authentication/change-password', function(){
+            return View('admin.tailstack.authentication.change-password');
+        });
+        Route::get('authentication/logout-page', function(){
+            return View('admin.tailstack.authentication.logout-page');
+        });
+        Route::get('pages-profile', function(){
+            return View('admin.tailstack.pages-profile');
+        });
+        Route::get('pages-service', function(){
+            return View('admin.tailstack.pages-service');
+        });
+        Route::get('pages-faq', function(){
+            return View('admin.tailstack.pages-faq');
+        });
+        Route::get('pages-pricing', function(){
+            return View('admin.tailstack.pages-pricing');
+        });
+        Route::get('pages-maintenance', function(){
+            return View('admin.tailstack.pages-maintenance');
+        });
+        Route::get('pages-404', function(){
+            return View('admin.tailstack.pages-404');
+        });
+        Route::get('pages-starter', function(){
+            return View('admin.tailstack.pages-starter');
+        });
+        Route::get('landing', function(){
+            return View('admin.tailstack.landing');
+        });
+        Route::get('layout-sidedark', function(){
+            return View('admin.tailstack.layout-sidedark');
+        });
+        Route::get('layout-dark', function(){
+            return View('admin.tailstack.layout-dark');
+        });
+        Route::get('layout-rtl', function(){
+            return View('admin.tailstack.layout-rtl');
+        });
+        Route::get('layout-compact', function(){
+            return View('admin.tailstack.layout-compact');
+        });
+        Route::get('layout-topnav', function(){
+            return View('admin.tailstack.layout-topnav');
+        });
+        Route::get('layout-preloader', function(){
+            return View('admin.tailstack.layout-preloader');
+        });
+        Route::get('widgets', function(){
+            return View('admin.tailstack.widgets');
+        });
+        Route::get('components/accordion', function(){
+            return View('admin.tailstack.components.accordion');
+        });
+        Route::get('components/alerts', function(){
+            return View('admin.tailstack.components.alerts');
+        });
+        Route::get('components/badge', function(){
+            return View('admin.tailstack.components.badge');
+        });
+        Route::get('components/breadcrumb', function(){
+            return View('admin.tailstack.components.breadcrumb');
+        });
+        Route::get('components/buttons', function(){
+            return View('admin.tailstack.components.buttons');
+        });
+        Route::get('components/card', function(){
+            return View('admin.tailstack.components.card');
+        });
+        Route::get('components/carousel', function(){
+            return View('admin.tailstack.components.carousel');
+        });
+        Route::get('components/accordion', function(){
+            return View('admin.tailstack.components.accordion');
+        });
+        Route::get('components/collapse', function(){
+            return View('admin.tailstack.components.collapse');
+        });
+        Route::get('components/devices', function(){
+            return View('admin.tailstack.components.devices');
+        });
+        Route::get('components/dropdowns', function(){
+            return View('admin.tailstack.components.dropdowns');
+        });
+        Route::get('components/grid', function(){
+            return View('admin.tailstack.components.grid');
+        });
+        Route::get('components/list-group', function(){
+            return View('admin.tailstack.components.list-group');
+        });
+        Route::get('components/lightbox', function(){
+            return View('admin.tailstack.components.lightbox');
+        });
+        Route::get('components/map', function(){
+            return View('admin.tailstack.components.map');
+        });
+        Route::get('components/modal', function(){
+            return View('admin.tailstack.components.modal');
+        });
+        Route::get('components/avatar', function(){
+            return View('admin.tailstack.components.avatar');
+        });
+        Route::get('components/navbar', function(){
+            return View('admin.tailstack.components.navbar');
+        });
+        Route::get('components/notification', function(){
+            return View('admin.tailstack.components.notification');
+        });
+        Route::get('components/offcanvas', function(){
+            return View('admin.tailstack.components.offcanvas');
+        });
+        Route::get('components/pagination', function(){
+            return View('admin.tailstack.components.pagination');
+        });
+        Route::get('components/popovers', function(){
+            return View('admin.tailstack.components.popovers');
+        });
+        Route::get('components/progress', function(){
+            return View('admin.tailstack.components.progress');
+        });
+        Route::get('components/ribbon', function(){
+            return View('admin.tailstack.components.ribbon');
+        });
+        Route::get('components/scrollbar', function(){
+            return View('admin.tailstack.components.scrollbar');
+        });
+        Route::get('components/spinners', function(){
+            return View('admin.tailstack.components.spinners');
+        });
+        Route::get('components/sweetalert', function(){
+            return View('admin.tailstack.components.sweetalert');
+        });
+
+        Route::get('components/tabs', function(){
+            return View('admin.tailstack.components.tabs');
+        });
+        Route::get('components/tooltips', function(){
+            return View('admin.tailstack.components.tooltips');
+        });
+        Route::get('forms/basic', function(){
+            return View('admin.tailstack.forms.basic');
+        });
+        Route::get('forms/datepicker', function(){
+            return View('admin.tailstack.forms.datepicker');
+        });
+        Route::get('forms/editor', function(){
+            return View('admin.tailstack.forms.editor');
+        });
+        Route::get('forms/input-tags', function(){
+            return View('admin.tailstack.forms.input-tags');
+        });
+        Route::get('forms/uploader', function(){
+            return View('admin.tailstack.forms.uploader');
+        });
+        Route::get('forms/validation', function(){
+            return View('admin.tailstack.forms.validation');
+        });
+        Route::get('forms/layout', function(){
+            return View('admin.tailstack.forms.layout');
+        });
+        Route::get('content/typography', function(){
+            return View('admin.tailstack.content.typography');
+        });
+        Route::get('content/tables', function(){
+            return View('admin.tailstack.content.tables');
+        });
+        Route::get('content/images', function(){
+            return View('admin.tailstack.content.images');
+        });
+        Route::get('content/embed-video', function(){
+            return View('admin.tailstack.content.embed-video');
+        });
+        Route::get('content/icons', function(){
+            return View('admin.tailstack.content.icons');
+        });
+        Route::get('charts', function(){
+            return View('admin.tailstack.charts');
+        });
+        Route::get('file-manager', function(){
+            return View('admin.tailstack.file-manager');
+        });
+        Route::get('docs/introduction', function(){
+            return View('admin.tailstack.docs.introduction');
+        });
+        Route::get('docs/tools', function(){
+            return View('admin.tailstack.docs.tools');
+        });
+        Route::get('docs/customize', function(){
+            return View('admin.tailstack.docs.customize');
+        });
+        Route::get('docs/rtl', function(){
+            return View('admin.tailstack.docs.rtl');
+        });
+        Route::get('docs/credits', function(){
+            return View('admin.tailstack.docs.credits');
+        });
+        Route::get('docs/changelog', function(){
+            return View('admin.tailstack.docs.changelog');
+        });
+
 
     });
     
