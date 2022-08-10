779a016cbfab91dd8259e6f236e3e43f350c4ba8
ExoticAmmu <izavhishek@gmail.com>
2022-08-10 22:28:49 +0530

    Header Fixed

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,7 +8,7 @@
                 <!-- logo -->
                 <a class="flex items-center py-2 mr-4 text-xl" href="index.html">
                     <h2 class="text-2xl font-semibold text-gray-200 px-4 max-h-9 overflow-hidden">
-                        <img class="inline-block w-7 h-auto ltr:mr-2 rtl:ml-2 -mt-1" src="/img/logo.png">
+                        <img class="inline-block w-7 h-auto ltr:mr-2 rtl:ml-2 -mt-1" src="/img/logo.svg">
                         <span class="text-gray-200">Flipozo</span>
                     </h2>
                 </a>
@@ -62,7 +62,7 @@
                         <div class="mh-18 text-center px-12 mb-8">
                             <a href="landing.html#" class="flex relative">
                                 <h2 class="text-2xl font-semibold text-gray-200 max-h-9">
-                                    <img class="inline-block w-7 h-auto ltr:mr-2 rtl:ml-2 -mt-1" src="/img/logo.png">
+                                    <img class="inline-block w-7 h-auto ltr:mr-2 rtl:ml-2 -mt-1" src="/img/logo.svg">
                                     <span class="text-gray-700 dark:text-gray-200">Taildash</span>
                                 </h2>
                             </a>
@@ -157,29 +157,29 @@
                 <!-- menu -->
                 <ul class="flex flex-col lg:mx-auto mt-2 lg:flex-row lg:mt-0 text-gray-700">
                     <li class="relative">
-                        <a class="block py-3 lg:py-7 px-6 hover:text-white focus:text-white" href="landing.html#">
+                        <a class="block py-3 lg:py-7 px-4 hover:text-white focus:text-white" href="landing.html#">
                             Home
                         </a>
                     </li>
                     <li class="relative">
-                        <a class="block py-3 lg:py-7 px-6 hover:text-white focus:text-white" href="landing.html#">
+                        <a class="block py-3 lg:py-7 px-4 hover:text-white focus:text-white" href="landing.html#">
                             Features
                         </a>
                     </li>
                     <li class="relative">
-                        <a class="block py-3 lg:py-7 px-6 hover:text-white focus:text-white" href="landing.html#">
+                        <a class="block py-3 lg:py-7 px-4 hover:text-white focus:text-white" href="landing.html#">
                             Pricing
                         </a>
                     </li>
                     <li class="relative">
-                        <a class="block py-3 lg:py-7 px-6 hover:text-white focus:text-white" href="landing.html#">
+                        <a class="block py-3 lg:py-7 px-4 hover:text-white focus:text-white" href="landing.html#">
                             Faq
                         </a>
                     </li>
                     <!-- dropdown -->
                     <li class="relative" x-data="{ open: false }" @keydown.escape.stop="open = false"
                         @mouseleave="open = false">
-                        <a id="dropdown-01" class="block py-3 lg:py-7 px-6 hover:text-white focus:text-white"
+                        <a id="dropdown-01" class="block py-3 lg:py-7 px-4 hover:text-white focus:text-white"
                             href="javascript:;" @mouseenter="open = !open" aria-haspopup="true"
                             x-bind:aria-expanded="open" :class="{ 'text-white': open }">
                             Docs
@@ -216,28 +216,73 @@
                         </ul>
                     </li>
                     <li class="relative">
-                        <a class="block py-3 lg:py-7 px-6 hover:text-white focus:text-white" href="landing.html#">
+                        <a class="block py-3 lg:py-7 px-4 hover:text-white focus:text-white" href="landing.html#">
                             Contact
                         </a>
                     </li>
                 </ul>
+                <div class="hidden xl:flex max-w-xs py-2 pl-4 pr-2 bg-white rounded-lg border">
+                    <svg class="w-8 h-10 mr-2" width="18" height="18" viewbox="0 0 18 18" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M17.5 17.1309L12.5042 11.9551" stroke="black" stroke-miterlimit="10"
+                            stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path
+                            d="M7.27524 13.8672C10.8789 13.8672 13.8002 10.945 13.8002 7.34033C13.8002 3.73565 10.8789 0.813477 7.27524 0.813477C3.67159 0.813477 0.750244 3.73565 0.750244 7.34033C0.750244 10.945 3.67159 13.8672 7.27524 13.8672Z"
+                            stroke="black" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round">
+                        </path>
+                    </svg>
+                    <input class="w-full border-0 focus:ring-transparent focus:outline-none py-2" type="text"
+                        placeholder="">
+                    <select
+                        class="pl-6 pr-6 border-0 border-l border-gray-100 focus:border-gray-100 focus:ring-transparent focus:outline-none cursor-pointer"
+                        name="" id="">
+                        <option>All&nbsp;items</option>
+                        <option>Collection</option>
+                        <option>Option</option>
+                    </select>
+                </div>
 
-                <!-- button -->
-                <div class="grid text-center lg:block my-4 lg:my-auto">
-                    <a class="py-2 px-4 text-sm inline-block text-center rounded leading-5 text-gray-100 bg-pink-500 border border-pink-500 hover:text-gray-300 hover:bg-pink-600 hover:ring-0 hover:border-pink-600 focus:bg-pink-600 focus:border-pink-600 focus:outline-none focus:ring-0"
-                        target="_blank" rel="noopener" href="landing.html#">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="inline ltr:mr-1 rtl:ml-1" width="1.2rem"
-                            height="1.2rem" fill="currentColor" viewBox="0 0 512 512">
-                            <circle cx="176" cy="416" r="16"
-                                style="fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" />
-                            <circle cx="400" cy="416" r="16"
-                                style="fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" />
-                            <polyline points="48 80 112 80 160 352 416 352"
-                                style="fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" />
-                            <path d="M160,288H409.44a8,8,0,0,0,7.85-6.43l28.8-144a8,8,0,0,0-7.85-9.57H128"
-                                style="fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" />
-                        </svg> Buy Now
+                <div class="flex mb-8 justify-between">
+                    <a class="inline-flex items-center font-semibold font-heading" href="#">
+                        <svg class="mr-3" width="32" height="31" viewbox="0 0 32 31" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M16.0006 16.3154C19.1303 16.3154 21.6673 13.799 21.6673 10.6948C21.6673 7.59064 19.1303 5.07422 16.0006 5.07422C12.871 5.07422 10.334 7.59064 10.334 10.6948C10.334 13.799 12.871 16.3154 16.0006 16.3154Z"
+                                stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                                stroke-linejoin="round"></path>
+                            <path
+                                d="M24.4225 23.8963C23.6678 22.3507 22.4756 21.0445 20.9845 20.1298C19.4934 19.2151 17.7647 18.7295 15.9998 18.7295C14.2349 18.7295 12.5063 19.2151 11.0152 20.1298C9.52406 21.0445 8.33179 22.3507 7.57715 23.8963"
+                                stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                                stroke-linejoin="round"></path>
+                        </svg>
+                        <span>Sign&nbsp;In</span>
                     </a>
+                    <div class="flex items-center">
+                        <a class="mr-10" href="#">
+                            <svg width="23" height="20" viewbox="0 0 23 20" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M11.4998 19.2061L2.70115 9.92527C1.92859 9.14433 1.41864 8.1374 1.24355 7.04712C1.06847 5.95684 1.23713 4.8385 1.72563 3.85053V3.85053C2.09464 3.10462 2.63366 2.45803 3.29828 1.96406C3.9629 1.47008 4.73408 1.14284 5.5483 1.00931C6.36252 0.875782 7.19647 0.939779 7.98144 1.19603C8.7664 1.45228 9.47991 1.89345 10.0632 2.48319L11.4998 3.93577L12.9364 2.48319C13.5197 1.89345 14.2332 1.45228 15.0182 1.19603C15.8031 0.939779 16.6371 0.875782 17.4513 1.00931C18.2655 1.14284 19.0367 1.47008 19.7013 1.96406C20.3659 2.45803 20.905 3.10462 21.274 3.85053V3.85053C21.7625 4.8385 21.9311 5.95684 21.756 7.04712C21.581 8.1374 21.071 9.14433 20.2984 9.92527L11.4998 19.2061Z"
+                                    stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                                    stroke-linejoin="round"></path>
+                            </svg>
+                        </a>
+                        <a class="flex items-center" href="#">
+                            <svg class="mr-3" width="23" height="23" viewbox="0 0 23 23" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M18.1159 8.72461H2.50427C1.99709 8.72461 1.58594 9.12704 1.58594 9.62346V21.3085C1.58594 21.8049 1.99709 22.2074 2.50427 22.2074H18.1159C18.6231 22.2074 19.0342 21.8049 19.0342 21.3085V9.62346C19.0342 9.12704 18.6231 8.72461 18.1159 8.72461Z"
+                                    stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                                    stroke-linejoin="round"></path>
+                                <path
+                                    d="M6.34473 6.34469V4.95676C6.34473 3.85246 6.76252 2.79338 7.5062 2.01252C8.24988 1.23165 9.25852 0.792969 10.3102 0.792969C11.362 0.792969 12.3706 1.23165 13.1143 2.01252C13.858 2.79338 14.2758 3.85246 14.2758 4.95676V6.34469"
+                                    stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                                    stroke-linejoin="round"></path>
+                            </svg>
+                            <span
+                                class="inline-block w-6 h-6 text-center bg-gray-100 rounded-full font-semibold font-heading">3</span>
+                        </a>
+                    </div>
                 </div>
             </div><!-- end desktop menu -->
         </div>
